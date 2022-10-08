821ddac92dabb2add59d45919ce78369074963c6
owlCode <akoladeo890@yahoo.com>
2022-10-08 12:17:09 +0100

    did some refactoring

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-//let brandSection = document.querySelectorAll('*[class*="section"]');
-let brandSection = document.querySelectorAll('main > section:not(:first-child)');
-console.log(brandSection);
+const hamburger = document.querySelector(".header__checkbox");
 
-
-function obsCallback(entries, observer) {
-  let [entry] = entries;
-  if (entry.isIntersecting === false) return;
-  console.log(entry.target);
-  entry.target.classList.add('display');
-  entry.target.style.opacity = '1';
-  observer.unobserve(entry.target)
-  console.log(entry);
-}
-
-const options = {
-  root: null,
-  threshold: 0.1,
-
-}
-let observer = new IntersectionObserver(obsCallback, options);
-
-brandSection.forEach(bs => {
-  observer.observe(bs);
-  bs.style.opacity = '0';
+hamburger.addEventListener("click", () => {
+   document.body.classList.toggle("scroll-off");
 });
-
-
